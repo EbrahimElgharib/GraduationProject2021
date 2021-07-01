5d865ad27d5d60c41d66e10e8a9af78a5d7cc615
EbrahimStd <ibrahim267745@eng.zu.edu.eg>
2021-07-01 05:23:16 +0200

    edit

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'virtual-lab-2021.herokuapp.com',
-    'https://virtual-lab-2021.herokuapp.com',
+    # 'https://virtual-lab-2021.herokuapp.com',
     '127.0.0.1',
     'localhost',
     ]
