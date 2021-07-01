1cd68a80645a5a87fb2dc131bb7e10b6a61c0624
EbrahimStd <ibrahim267745@eng.zu.edu.eg>
2021-07-01 22:48:53 +0200

    edit

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ app_name = 'accounts'
 urlpatterns= [
    path('signup/', views.signup, name='signup'),
    path('profile/', views.profile, name='profile'),
-   path('profile/edit', views.edit_profile, name='edit_profile'),
+   path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
--- project/settings.py
+++ project/settings.py
@@ -53,6 +53,10 @@ INSTALLED_APPS = [
     # providers
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.twitter',
+    'allauth.socialaccount.providers.linkedin_oauth2',
+    
 
     ##### FrontEnd
     # https://pypi.org/project/django-bootstrap4/
@@ -73,6 +77,14 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1 # for social accounts / allauth
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
@@ -106,7 +118,34 @@ SOCIALACCOUNT_PROVIDERS = {
         'AUTH_PARAMS': {
             'access_type': 'online',
         }
-    }
+    },
+
+    # 'github': {
+    #     'SCOPE': [
+    #         'user',
+    #         'repo',
+    #         'read:org',
+    #     ],
+    # },
+
+    'linkedin_oauth2': {
+        'SCOPE': [
+            'r_basicprofile',
+            'r_emailaddress'
+        ],
+        'PROFILE_FIELDS': [
+            'id',
+            'first-name',
+            'last-name',
+            'email-address',
+            'picture-url',
+            'public-profile-url',
+        ]
+    },
+
+
+
+
 }
 
 
@@ -141,6 +180,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'settings.footer_context_processor.myfooter', # my footer
+                
             ],
         },
     },
@@ -162,6 +202,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
