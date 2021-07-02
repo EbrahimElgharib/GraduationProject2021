d696633a291e28d48a3122aed2d6459c2b5286a7
omarsalah331 <omarsalah331@gmail.com>
2021-07-02 03:23:44 +0200

    labs first draft, some models will be added, pagination will ba added

--- labs/models.py
+++ labs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-
+from django.utils.text import slugify
+from django.urls import reverse
 # Create your models here.
 
 ### labs
@@ -14,9 +15,22 @@ class Lab(models.Model):
    # FK
    category = models.ForeignKey('Category', related_name='lab_category', on_delete=models.CASCADE)
 
+   # feature (slug) for url with title of post_detail
+   slug = models.SlugField(null=True, blank=True)
+   # for slug - save slug field = title of post
+   def save(self, *args, **kwargs):
+       if not self.slug:
+          self.slug = slugify(self.title)
+        # Call the real save() method
+       super(Lab, self).save(*args, **kwargs)
+
    def __str__(self):
        return self.title
-            
+
+   def get_absolute_url(self):
+       return reverse('labs:lab_detail',args=[str(self.slug)])
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
--- labs/urls.py
+++ labs/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'blog'
+app_name = 'labs'
 
 urlpatterns = [
     path('', views.LabList.as_view(), name='lab_list'),
-
+    path('<slug:slug>',views.LabDetail.as_view(),name='lab_detail'),
 ]
