412831faff332fa59bdc266e75e9236e0202d883
EbrahimStd <ibrahim267745@eng.zu.edu.eg>
2021-07-04 09:40:31 +0200

    add alert in home page when confirm or delete subscriber

--- settings/views.py
+++ settings/views.py
@@ -71,15 +71,15 @@ def confirm(request):
     if sub.conf_num == request.GET['conf_num']:
         sub.confirmed = True
         sub.save()
-        return render(request, 'subscriber/subscribe.html', {'email': sub.email, 'action': 'confirmed'})
+        return render(request, 'settings/home.html', {'email': sub.email, 'action': 'confirmed'})
     else:
-        return render(request, 'subscriber/subscribe.html', {'email': sub.email, 'action': 'denied'})
+        return render(request, 'settings/home.html', {'email': sub.email, 'action': 'denied'})
 
 # delete subscribers
 def delete(request):
     sub = Subscriber.objects.get(email=request.GET['email'])
     if sub.conf_num == request.GET['conf_num']:
         sub.delete()
-        return render(request, 'subscriber/subscribe.html', {'email': sub.email, 'action': 'unsubscribed'})
+        return render(request, 'settings/home.html', {'email': sub.email, 'action': 'unsubscribed'})
     else:
-        return render(request, 'subscriber/subscribe.html', {'email': sub.email, 'action': 'denied'})
+        return render(request, 'settings/home.html', {'email': sub.email, 'action': 'denied'})
