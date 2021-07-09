572bef11cc6bd99c9c42161315264abb24edb7c8
EbrahimStd <ibrahim267745@eng.zu.edu.eg>
2021-07-09 07:20:08 +0200

    add but can not send

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,6 @@ app_name = 'accounts'
 urlpatterns= [
    path('signup/', views.signup, name='signup'),
    path('profile/', views.profile, name='profile'),
+
+   path('activate/<uidb64>/<token>', views.VerificationView.as_view(), name='activate'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,18 @@ from django.contrib.auth.decorators import login_required
 from .forms import SignupForm, ProfileForm, UserForm
 from .models import Profile
 
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from .utils import account_activation_token
+from django.urls import reverse
+
 # Create your views here.
 
 def signup(request):
@@ -16,21 +28,89 @@ def signup(request):
       form = SignupForm(request.POST)
       # check if form data is valid before save in DB
       if form.is_valid():
-         form.save()
-         # login after save
-         username = form.cleaned_data['username']
-         password = form.cleaned_data['password1']
+         # form.save()
+         # # login after save
+         # username = form.cleaned_data['username']
+         # password = form.cleaned_data['password1']
          print('1')
-         user = authenticate(username=username, password=password)
+         # user = authenticate(username=username, password=password)
+         # print('2')
+         # login(request, user)
+         # print('3')
+         # return redirect('/accounts/profile')
+
+         #test
+         username = request.POST['username']
+         email = request.POST['email']
+         password = request.POST['password1']
          print('2')
-         login(request, user)
-         print('3')
-         return redirect('/accounts/profile')
+
+         if not User.objects.filter(username=username).exists():
+             print('3')
+             if not User.objects.filter(email=email).exists():
+                 print('4')
+                 user = User.objects.create_user(username=username, email=email)
+                 user.set_password(password)
+                 user.is_active = False
+                 user.save()
+                 current_site = get_current_site(request)
+                 print('5')
+                 email_body = {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                }
+
+                 print('6') 
+                 link = reverse('accounts:activate', kwargs={
+                               'uidb64': email_body['uid'], 'token': email_body['token']})
+                 print('7')
+                 email_subject = 'Activate your account'
+
+                 activate_url = 'http://'+current_site.domain+link
+
+                 email = EmailMessage(
+                    email_subject,
+                    'Hi '+user.username + ', Please the link below to activate your account \n'+activate_url,
+                    'ebrahimtest44@gmail.com',
+                    [email],
+                )
+                 email.send(fail_silently=False)
+                 messages.success(request, 'Account successfully created')
+                 return render(request, 'registration/signup.html')
+
+         return render(request, 'registration/signup.html')
+
           
    else: # else return form as empty
       form = SignupForm()
       
    return render(request,'registration/signup.html',{'form':form})
+
+
+
+class VerificationView(View):
+    def get(self, request, uidb64, token):
+        try:
+            id = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=id)
+
+            if not account_activation_token.check_token(user, token):
+                return redirect('login'+'?message='+'User already activated')
+
+            if user.is_active:
+                return redirect('login')
+            user.is_active = True
+            user.save()
+
+            messages.success(request, 'Account activated successfully')
+            return redirect('login')
+
+        except Exception as ex:
+            pass
+
+        return redirect('login')
 
 # # show profile data to user
 # @login_required
