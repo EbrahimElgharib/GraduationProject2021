21f3f2d3cf99a2e333429e8f05e0f574757917f0
EbrahimStd <ibrahim267745@eng.zu.edu.eg>
2021-07-09 13:56:40 +0200

    edit user authenticated with login&signup pages

--- project/settings.py
+++ project/settings.py
@@ -259,10 +259,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 ##### login
 LOGIN_URL = '/accounts/login'
-
 ##### Redirect to home URL after login (Default redirects to /accounts/profile/)
 # LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/accounts/profile'
+
 
 
 
