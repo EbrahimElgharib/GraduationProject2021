791ca2305689f7efcd4ae0b11cd9228487bedae5
EbrahimStd <ibrahim267745@eng.zu.edu.eg>
2021-07-14 15:07:43 +0200

    edit

--- project/urls.py
+++ project/urls.py
@@ -18,10 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-
-settings
-
-
 urlpatterns = [
     # accounts must be at first of all
     path('accounts/', include('django.contrib.auth.urls')), # User Auth
@@ -40,5 +36,6 @@ urlpatterns = [
 
 ]
 
+# settings for static and media roots
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
